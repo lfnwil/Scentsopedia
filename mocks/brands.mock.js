c8af992b126cdc26c35b1e43979f6f0672bec808
william <william@batiecom.com>
2026-06-25 14:29:30 +0200

    models

--- mocks/brands.mock.js
+++ mocks/brands.mock.js
@@ -1,4 +1,9 @@
 export const brandsMocks = [
   { name: "Maison Crivelli", img: "https://fimgs.net/mdimg/perfume/375x500.90733.jpg" },
   { name: "Dior", img: "https://example.com/dior.jpg" },
+  { name: "Maison Francis Kurkdjian", img: "https://example.com/mfk.jpg" },
+  { name: "Byredo", img: "https://example.com/byredo.jpg" },
+  { name: "Le Labo", img: "https://example.com/le-labo.jpg" },
+  { name: "Xerjoff", img: "https://example.com/xerjoff.jpg" },
+  { name: "Nishane", img: "https://example.com/nishane.jpg" },
 ];
